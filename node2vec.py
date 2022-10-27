c97fb3c697fc73da9b93998c67112e70e1fdbd01
Brandon Morrell <brandon@morrellworld.net>
2022-10-27 13:59:25 -0500

    cleaned up graph

--- node2vec.py
+++ node2vec.py
@@ -1,10 +1,9 @@
 class Node2Vec:
 
-
-    #p -- return rate
-    #q -- exploration rate
-    #random_walk_lengths -- number of steps taken in each random walk
-    #num_walks -- number of random walks completed.
+    # p -- return rate
+    # q -- exploration rate
+    # random_walk_lengths -- number of steps taken in each random walk
+    # num_walks -- number of random walks completed.
 
     def __init__(self, node_graph, q, p, random_walk_length, num_walks):
         self.node_graph = node_graph
@@ -14,4 +13,5 @@ class Node2Vec:
         self.num_walks = num_walks
 
     def random_walk(self):
-        print('hi')
+        pass
+        # at each node, new exploration should be 1/q, return to previous node should be 1/p
