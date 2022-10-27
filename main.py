a6fa843bb3ccf234cd1e721288a2fc030acdc012
Brandon Morrell <brandon@morrellworld.net>
2022-10-27 12:48:46 -0500

    Added randomly generated graph + visual

---
+++ main.py
@@ -0,0 +1,72 @@
+import networkx as nx
+from node2vec import Node2Vec
+import plotly.graph_objects as go
+
+graph = nx.random_geometric_graph(200, 0.125)
+
+node2vec = Node2Vec(graph, 1, 1, 10, 100)
+
+edge_x = []
+edge_y = []
+for edge in graph.edges():
+    x0, y0 = graph.nodes[edge[0]]['pos']
+    x1, y1 = graph.nodes[edge[1]]['pos']
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None)
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
+
+edge_trace = go.Scatter(
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
+
+node_x = []
+node_y = []
+for node in graph.nodes():
+    x, y = graph.nodes[node]['pos']
+    node_x.append(x)
+    node_y.append(y)
+
+
+
+node_trace = go.Scatter(
+    x=node_x, y=node_y,
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        colorscale='YlGnBu',
+        reversescale=True,
+        color=[],
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'
+        ),
+        line_width=2))
+
+node_adjacencies = []
+node_text = []
+for node, adjacencies in enumerate(graph.adjacency()):
+    node_adjacencies.append(len(adjacencies[1]))
+    node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+node_trace.marker.color = node_adjacencies
+node_trace.text = node_text
+
+fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+fig.show()
---
+++ node2vec.py
@@ -0,0 +1,17 @@
+class Node2Vec:
+
+
+    #p -- return rate
+    #q -- exploration rate
+    #random_walk_lengths -- number of steps taken in each random walk
+    #num_walks -- number of random walks completed.
+
+    def __init__(self, node_graph, q, p, random_walk_length, num_walks):
+        self.node_graph = node_graph
+        self.q = q
+        self.p = p
+        self.random_walk_length = random_walk_length
+        self.num_walks = num_walks
+
+    def random_walk(self):
+        print('hi')
