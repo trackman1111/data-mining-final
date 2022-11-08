898624e4333d14eb226c8fa6c00fec167eee5f55
Brandon Morrell <brandon@morrellworld.net>
2022-11-08 08:20:01 -0800

    pushed correct version

--- main.py
+++ main.py
@@ -1,99 +1,51 @@
-import argparse
-import numpy as np
 import networkx as nx
+import numpy as np
+
 import node2vec
 from gensim.models import Word2Vec
-
-
-def parse_args():
-    '''
-    Parses the node2vec arguments.
-    '''
-    parser = argparse.ArgumentParser(description="Run node2vec.")
-
-    parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
-                        help='Input graph path')
-
-    parser.add_argument('--output', nargs='?', default='emb/karate.emb',
-                        help='Embeddings path')
-
-    parser.add_argument('--dimensions', type=int, default=128,
-                        help='Number of dimensions. Default is 128.')
-
-    parser.add_argument('--walk-length', type=int, default=80,
-                        help='Length of walk per source. Default is 80.')
-
-    parser.add_argument('--num-walks', type=int, default=10,
-                        help='Number of walks per source. Default is 10.')
-
-    parser.add_argument('--window-size', type=int, default=10,
-                        help='Context size for optimization. Default is 10.')
-
-    parser.add_argument('--iter', default=1, type=int,
-                        help='Number of epochs in SGD')
-
-    parser.add_argument('--workers', type=int, default=8,
-                        help='Number of parallel workers. Default is 8.')
-
-    parser.add_argument('--p', type=float, default=1,
-                        help='Return hyperparameter. Default is 1.')
-
-    parser.add_argument('--q', type=float, default=1,
-                        help='Inout hyperparameter. Default is 1.')
-
-    parser.add_argument('--weighted', dest='weighted', action='store_true',
-                        help='Boolean specifying (un)weighted. Default is unweighted.')
-    parser.add_argument('--unweighted', dest='unweighted', action='store_false')
-    parser.set_defaults(weighted=False)
-
-    parser.add_argument('--directed', dest='directed', action='store_true',
-                        help='Graph is (un)directed. Default is undirected.')
-    parser.add_argument('--undirected', dest='undirected', action='store_false')
-    parser.set_defaults(directed=False)
-
-    return parser.parse_args()
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
 
 def read_graph():
-    '''
-    Reads the input network in networkx.
-    '''
-    if args.weighted:
-        G = nx.read_edgelist(args.input, nodetype=int, data=(('weight', float),), create_using=nx.DiGraph())
-    else:
-        G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
-        for edge in G.edges():
-            G[edge[0]][edge[1]]['weight'] = 1
-
-    if not args.directed:
-        G = G.to_undirected()
+    G = nx.read_edgelist('nfl.edgelist', nodetype=str, create_using=nx.DiGraph())
+    for edge in G.edges():
+        G[edge[0]][edge[1]]['weight'] = 1
+    G = G.to_undirected()
 
     return G
 
 
 def learn_embeddings(walks):
-    '''
-    Learn embeddings by optimizing the Skipgram objective using SGD.
-    '''
-    walks = [map(str, walk) for walk in walks]
-    model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers,
-                     iter=args.iter)
-    model.save_word2vec_format(args.output)
-
-    return
+    walks = [list(map(str, walk)) for walk in walks]
+    model = Word2Vec(walks, vector_size=128, window=5, min_count=1, workers=8)
+    model.save('nfl.model')
+    return model
 
 
-def main(args):
-    '''
-    Pipeline for representational learning for all nodes in a graph.
-    '''
+def main():
     nx_G = read_graph()
-    G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
+    G = node2vec.Graph(nx_G, p=1, q=1)
     G.preprocess_transition_probs()
-    walks = G.simulate_walks(args.num_walks, args.walk_length)
-    learn_embeddings(walks)
+    walks = G.simulate_walks(num_walks=500, walk_length=100)
+    model = learn_embeddings(walks)
+    print(model.wv.most_similar("WAS"))
+    nodes = [x for x in list(model.wv.key_to_index.keys())]
+    embeddings = np.array([model.wv[x] for x in nodes])
+
+    tsne = TSNE(n_components=2, random_state=7, perplexity=15)
+    embeddings_2d = tsne.fit_transform(embeddings)
+
+    figure = plt.figure(figsize=(11, 9))
+    ax = figure.add_subplot(111)
+
+    for i, word in enumerate(nodes):
+        plt.annotate(word, xy=(embeddings_2d[i, 0], embeddings_2d[i, 1]))
+
+    ax.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1])
+
+    plt.savefig('test.png')
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    main()
--- node2vec.py
+++ node2vec.py
@@ -1,19 +1,14 @@
 import numpy as np
-import networkx as nx
 import random
 
 
 class Graph():
-    def __init__(self, nx_G, is_directed, p, q):
+    def __init__(self, nx_G, p, q):
         self.G = nx_G
-        self.is_directed = is_directed
         self.p = p
         self.q = q
 
     def node2vec_walk(self, walk_length, start_node):
-        '''
-		Simulate a random walk starting from start node.
-		'''
         G = self.G
         alias_nodes = self.alias_nodes
         alias_edges = self.alias_edges
@@ -37,15 +32,10 @@ class Graph():
         return walk
 
     def simulate_walks(self, num_walks, walk_length):
-        '''
-		Repeatedly simulate random walks from each node.
-		'''
         G = self.G
         walks = []
         nodes = list(G.nodes())
-        print('Walk iteration:')
         for walk_iter in range(num_walks):
-            print(str(walk_iter + 1), '/', str(num_walks))
             random.shuffle(nodes)
             for node in nodes:
                 walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
@@ -53,9 +43,6 @@ class Graph():
         return walks
 
     def get_alias_edge(self, src, dst):
-        '''
-		Get the alias edge setup lists for a given edge.
-		'''
         G = self.G
         p = self.p
         q = self.q
@@ -74,11 +61,8 @@ class Graph():
         return alias_setup(normalized_probs)
 
     def preprocess_transition_probs(self):
-        '''
-		Preprocessing of transition probabilities for guiding the random walks.
-		'''
+
         G = self.G
-        is_directed = self.is_directed
 
         alias_nodes = {}
         for node in G.nodes():
@@ -88,15 +72,11 @@ class Graph():
             alias_nodes[node] = alias_setup(normalized_probs)
 
         alias_edges = {}
-        triads = {}
 
-        if is_directed:
-            for edge in G.edges():
-                alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-        else:
-            for edge in G.edges():
-                alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-                alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
+
+        for edge in G.edges():
+            alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
+            alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
 
         self.alias_nodes = alias_nodes
         self.alias_edges = alias_edges
@@ -105,11 +85,7 @@ class Graph():
 
 
 def alias_setup(probs):
-    '''
-	Compute utility lists for non-uniform sampling from discrete distributions.
-	Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
-	for details
-	'''
+
     K = len(probs)
     q = np.zeros(K)
     J = np.zeros(K, dtype=np.int)
@@ -138,9 +114,6 @@ def alias_setup(probs):
 
 
 def alias_draw(J, q):
-    '''
-	Draw sample from a non-uniform discrete distribution using alias sampling.
-	'''
     K = len(J)
 
     kk = int(np.floor(np.random.rand() * K))
