0ebec56ee658b09f146e9e4245d34f3648f7c61e
Brandon Morrell <brandon@morrellworld.net>
2022-11-08 08:17:14 -0800

    Update before edges

--- main.py
+++ main.py
@@ -1,80 +1,99 @@
+import argparse
+import numpy as np
 import networkx as nx
-from node2vec import Node2Vec
-import plotly.graph_objects as go
+import node2vec
+from gensim.models import Word2Vec
 
 
-def main():
-    # initialize graph
-    graph = nx.random_geometric_graph(200, 0.125)
+def parse_args():
+    '''
+    Parses the node2vec arguments.
+    '''
+    parser = argparse.ArgumentParser(description="Run node2vec.")
 
-    # create node2vec object
-    node2vec = Node2Vec(graph, 1, 1, 10, 100)
-    graph_display(graph)
+    parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
+                        help='Input graph path')
 
+    parser.add_argument('--output', nargs='?', default='emb/karate.emb',
+                        help='Embeddings path')
 
-def graph_display(graph):
-    edge_x = []
-    edge_y = []
-    for edge in graph.edges():
-        x0, y0 = graph.nodes[edge[0]]['pos']
-        x1, y1 = graph.nodes[edge[1]]['pos']
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
+    parser.add_argument('--dimensions', type=int, default=128,
+                        help='Number of dimensions. Default is 128.')
 
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        line=dict(width=1, color='#888'),
-        hoverinfo='none',
-        mode='lines')
+    parser.add_argument('--walk-length', type=int, default=80,
+                        help='Length of walk per source. Default is 80.')
 
-    node_x = []
-    node_y = []
-    for node in graph.nodes():
-        x, y = graph.nodes[node]['pos']
-        node_x.append(x)
-        node_y.append(y)
+    parser.add_argument('--num-walks', type=int, default=10,
+                        help='Number of walks per source. Default is 10.')
 
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers',
-        hoverinfo='text',
-        marker=dict(
-            showscale=True,
-            colorscale='YlGnBu',
-            reversescale=True,
-            color=[],
-            size=10,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
-            line_width=2))
+    parser.add_argument('--window-size', type=int, default=10,
+                        help='Context size for optimization. Default is 10.')
 
-    node_adjacencies = []
-    node_text = []
-    for node, adjacencies in enumerate(graph.adjacency()):
-        node_adjacencies.append(len(adjacencies[1]))
-        node_text.append(str(len(adjacencies[1])))
+    parser.add_argument('--iter', default=1, type=int,
+                        help='Number of epochs in SGD')
 
-    node_trace.marker.color = node_adjacencies
-    node_trace.text = node_text
+    parser.add_argument('--workers', type=int, default=8,
+                        help='Number of parallel workers. Default is 8.')
 
-    fig = go.Figure(data=[edge_trace, node_trace],
-                    layout=go.Layout(
-                        titlefont_size=16,
-                        showlegend=False,
-                        hovermode='closest',
-                        margin=dict(b=20, l=5, r=5, t=40),
-                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-                    )
-    fig.show()
+    parser.add_argument('--p', type=float, default=1,
+                        help='Return hyperparameter. Default is 1.')
+
+    parser.add_argument('--q', type=float, default=1,
+                        help='Inout hyperparameter. Default is 1.')
+
+    parser.add_argument('--weighted', dest='weighted', action='store_true',
+                        help='Boolean specifying (un)weighted. Default is unweighted.')
+    parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+    parser.set_defaults(weighted=False)
+
+    parser.add_argument('--directed', dest='directed', action='store_true',
+                        help='Graph is (un)directed. Default is undirected.')
+    parser.add_argument('--undirected', dest='undirected', action='store_false')
+    parser.set_defaults(directed=False)
+
+    return parser.parse_args()
 
 
-main()
+def read_graph():
+    '''
+    Reads the input network in networkx.
+    '''
+    if args.weighted:
+        G = nx.read_edgelist(args.input, nodetype=int, data=(('weight', float),), create_using=nx.DiGraph())
+    else:
+        G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
+        for edge in G.edges():
+            G[edge[0]][edge[1]]['weight'] = 1
+
+    if not args.directed:
+        G = G.to_undirected()
+
+    return G
+
+
+def learn_embeddings(walks):
+    '''
+    Learn embeddings by optimizing the Skipgram objective using SGD.
+    '''
+    walks = [map(str, walk) for walk in walks]
+    model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers,
+                     iter=args.iter)
+    model.save_word2vec_format(args.output)
+
+    return
+
+
+def main(args):
+    '''
+    Pipeline for representational learning for all nodes in a graph.
+    '''
+    nx_G = read_graph()
+    G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
+    G.preprocess_transition_probs()
+    walks = G.simulate_walks(args.num_walks, args.walk_length)
+    learn_embeddings(walks)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
