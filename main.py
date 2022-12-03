6614f0125163adcc1e384863d62289aa2029efb5
Brandon Morrell <brandon@morrellworld.net>
2022-12-02 17:00:25 -0800

    Moved updated files

--- main.py
+++ main.py
@@ -27,9 +27,11 @@ def main():
     nx_G = read_graph()
     G = node2vec.Graph(nx_G, p=1, q=1)
     G.preprocess_transition_probs()
-    walks = G.simulate_walks(num_walks=500, walk_length=100)
+    walks = G.simulate_walks(num_walks=20000, walk_length=100)
     model = learn_embeddings(walks)
-    print(model.wv.most_similar("WAS"))
+    # print(model.wv.most_similar("WAS"))
+    # print(model.wv.most_similar("TEN"))
+
     nodes = [x for x in list(model.wv.key_to_index.keys())]
     embeddings = np.array([model.wv[x] for x in nodes])
 
@@ -42,9 +44,36 @@ def main():
     for i, word in enumerate(nodes):
         plt.annotate(word, xy=(embeddings_2d[i, 0], embeddings_2d[i, 1]))
 
-    ax.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1])
+    colors = []
+    nfc_east = ['WAS', 'NYG', 'PHI', 'DAL']
+    nfc_north = ['GB', 'MIN', 'DET', 'CHI']
+    nfc_south = ['NO', 'TB', 'CAR', 'ATL']
+    nfc_west = ['LA', 'SF', 'ARI', 'SEA']
+    afc_east = ['MIA', 'NYJ', 'NE', 'BUF']
+    afc_north = ['CIN', 'PIT', 'CLE', 'BAL']
+    afc_south = ['HOU', 'TEN', 'IND', 'JAX']
 
-    plt.savefig('test.png')
+    for node in nodes:
+        if node in nfc_east:
+            colors.append('green')
+        elif node in nfc_north:
+            colors.append('blue')
+        elif node in nfc_south:
+            colors.append('red')
+        elif node in nfc_west:
+            colors.append('teal')
+        elif node in afc_east:
+            colors.append('orange')
+        elif node in afc_north:
+            colors.append('purple')
+        elif node in afc_south:
+            colors.append('yellow')
+        else:
+            colors.append('pink')
+
+    ax.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=colors)
+
+    plt.savefig('scatter.png')
 
 
 if __name__ == "__main__":
