462f83a78c8974480c51e9803f520dcdde1dbec0
Brandon Morrell <brandon@morrellworld.net>
2022-12-06 10:40:58 -0800

    Updated node2vec file

--- main.py
+++ main.py
@@ -7,30 +7,29 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
 
-def read_graph():
-    G = nx.read_edgelist('nfl.edgelist', nodetype=str, create_using=nx.DiGraph())
-    for edge in G.edges():
-        G[edge[0]][edge[1]]['weight'] = 1
-    G = G.to_undirected()
+def create_weighted_graph():
+    graph = nx.read_edgelist('nfl.edgelist', nodetype=str, create_using=nx.DiGraph())
+    for edge in graph.edges():
+        graph[edge[0]][edge[1]]['weight'] = 1
+    graph = graph.to_undirected()
 
-    return G
+    return graph
 
 
 def learn_embeddings(walks):
     walks = [list(map(str, walk)) for walk in walks]
-    model = Word2Vec(walks, vector_size=128, window=5, min_count=1, workers=8)
+    model = Word2Vec(walks, vector_size=32, window=5, min_count=1, workers=8)
     model.save('nfl.model')
     return model
 
 
 def main():
-    nx_G = read_graph()
-    G = node2vec.Graph(nx_G, p=1, q=1)
-    G.preprocess_transition_probs()
-    walks = G.simulate_walks(num_walks=20000, walk_length=100)
+    nx_graph = create_weighted_graph()
+    graph = node2vec.Node2Vec(nx_graph, p=1, q=1)
+    graph.preprocess_edges()
+    graph.preprocess_nodes()
+    walks = graph.simulate_walks(num_walks=300, walk_length=10)
     model = learn_embeddings(walks)
-    # print(model.wv.most_similar("WAS"))
-    # print(model.wv.most_similar("TEN"))
 
     nodes = [x for x in list(model.wv.key_to_index.keys())]
     embeddings = np.array([model.wv[x] for x in nodes])
@@ -45,28 +44,36 @@ def main():
         plt.annotate(word, xy=(embeddings_2d[i, 0], embeddings_2d[i, 1]))
 
     colors = []
-    nfc_east = ['WAS', 'NYG', 'PHI', 'DAL']
-    nfc_north = ['GB', 'MIN', 'DET', 'CHI']
-    nfc_south = ['NO', 'TB', 'CAR', 'ATL']
-    nfc_west = ['LA', 'SF', 'ARI', 'SEA']
-    afc_east = ['MIA', 'NYJ', 'NE', 'BUF']
-    afc_north = ['CIN', 'PIT', 'CLE', 'BAL']
-    afc_south = ['HOU', 'TEN', 'IND', 'JAX']
+    divisions = {'nfc_east': ['WAS', 'NYG', 'PHI', 'DAL'], 'nfc_north': ['GB', 'MIN', 'DET', 'CHI'],
+                 'nfc_south': ['NO', 'TB', 'CAR', 'ATL'], 'nfc_west': ['LA', 'SF', 'ARI', 'SEA'],
+                 'afc_east': ['MIA', 'NYJ', 'NE', 'BUF'], 'afc_north': ['CIN', 'PIT', 'CLE', 'BAL'],
+                 'afc_south': ['HOU', 'TEN', 'IND', 'JAX'], 'afc_west': ['LAC', 'KC', 'DEN', 'LV']}
+
+    count = 0
+    correct = 0
+    for key in divisions:
+        for team in divisions[key]:
+            most_similar = model.wv.most_similar(team)
+            for x in range(0, 3):
+                if most_similar[x][0] in divisions[key]:
+                    correct += 1
+                count += 1
+    print("Correct Top 3 Relations: " + str(correct) + "/" + str(count))
 
     for node in nodes:
-        if node in nfc_east:
+        if node in divisions['nfc_east']:
             colors.append('green')
-        elif node in nfc_north:
+        elif node in divisions['nfc_north']:
             colors.append('blue')
-        elif node in nfc_south:
+        elif node in divisions['nfc_south']:
             colors.append('red')
-        elif node in nfc_west:
+        elif node in divisions['nfc_west']:
             colors.append('teal')
-        elif node in afc_east:
+        elif node in divisions['afc_east']:
             colors.append('orange')
-        elif node in afc_north:
+        elif node in divisions['afc_north']:
             colors.append('purple')
-        elif node in afc_south:
+        elif node in divisions['afc_west']:
             colors.append('yellow')
         else:
             colors.append('pink')
--- node2vec.py
+++ node2vec.py
@@ -2,99 +2,85 @@ import numpy as np
 import random
 
 
-class Graph():
-    def __init__(self, nx_G, p, q):
-        self.G = nx_G
+class Node2Vec:
+
+    def __init__(self, graph, p, q):
+        self.alias_edges = None
+        self.alias_nodes = None
+        self.graph = graph
         self.p = p
         self.q = q
 
-    def node2vec_walk(self, walk_length, start_node):
-        G = self.G
-        alias_nodes = self.alias_nodes
-        alias_edges = self.alias_edges
-
+    def random_walk(self, walk_length, start_node):
         walk = [start_node]
-
         while len(walk) < walk_length:
             cur = walk[-1]
-            cur_nbrs = sorted(G.neighbors(cur))
-            if len(cur_nbrs) > 0:
+            neighbors = list(self.graph.neighbors(cur))
+            if len(neighbors) > 0:
                 if len(walk) == 1:
-                    walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
+                    walk.append(neighbors[alias_draw(self.alias_nodes[cur][0], self.alias_nodes[cur][1])])
                 else:
-                    prev = walk[-2]
-                    next = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0],
-                                               alias_edges[(prev, cur)][1])]
-                    walk.append(next)
+                    prev_step = walk[-2]
+                    next_step = neighbors[alias_draw(self.alias_edges[(prev_step, cur)][0],
+                                                     self.alias_edges[(prev_step, cur)][1])]
+                    walk.append(next_step)
             else:
                 break
-
         return walk
 
     def simulate_walks(self, num_walks, walk_length):
-        G = self.G
         walks = []
-        nodes = list(G.nodes())
-        for walk_iter in range(num_walks):
-            random.shuffle(nodes)
+        nodes = list(self.graph.nodes())
+        for _ in range(num_walks):
             for node in nodes:
-                walks.append(self.node2vec_walk(walk_length=walk_length, start_node=node))
-
+                walks.append(self.random_walk(walk_length, node))
         return walks
 
-    def get_alias_edge(self, src, dst):
-        G = self.G
-        p = self.p
-        q = self.q
-
-        unnormalized_probs = []
-        for dst_nbr in sorted(G.neighbors(dst)):
-            if dst_nbr == src:
-                unnormalized_probs.append(G[dst][dst_nbr]['weight'] / p)
-            elif G.has_edge(dst_nbr, src):
-                unnormalized_probs.append(G[dst][dst_nbr]['weight'])
+    def calculate_edges(self, src, dst):
+        probs = []
+        for neighbors in self.graph.neighbors(dst):
+            if neighbors == src:
+                probs.append(self.graph[dst][neighbors]['weight'] / self.p)
+            elif self.graph.has_edge(neighbors, src):
+                probs.append(self.graph[dst][neighbors]['weight'])
             else:
-                unnormalized_probs.append(G[dst][dst_nbr]['weight'] / q)
-        norm_const = sum(unnormalized_probs)
-        normalized_probs = [float(u_prob) / norm_const for u_prob in unnormalized_probs]
+                probs.append(self.graph[dst][neighbors]['weight'] / self.q)
+        total_prob = sum(probs)
+        normalized_probs = [float(prob) / total_prob for prob in probs]
 
         return alias_setup(normalized_probs)
 
-    def preprocess_transition_probs(self):
-
-        G = self.G
-
-        alias_nodes = {}
-        for node in G.nodes():
-            unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
-            norm_const = sum(unnormalized_probs)
-            normalized_probs = [float(u_prob) / norm_const for u_prob in unnormalized_probs]
-            alias_nodes[node] = alias_setup(normalized_probs)
+    def preprocess_edges(self):
 
         alias_edges = {}
 
+        for edge in self.graph.edges():
+            alias_edges[edge] = self.calculate_edges(edge[0], edge[1])
+            alias_edges[(edge[1], edge[0])] = self.calculate_edges(edge[1], edge[0])
 
-        for edge in G.edges():
-            alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
-            alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
-
-        self.alias_nodes = alias_nodes
         self.alias_edges = alias_edges
 
-        return
+    def preprocess_nodes(self):
+        alias_nodes = {}
+        for node in self.graph.nodes():
+            probs = [self.graph[node][neighbor]['weight'] for neighbor in self.graph.neighbors(node)]
+            total_prob = sum(probs)
+            normalized_probs = [float(prob) / total_prob for prob in probs]
+            alias_nodes[node] = alias_setup(normalized_probs)
+
+        self.alias_nodes = alias_nodes
 
 
 def alias_setup(probs):
-
     K = len(probs)
     q = np.zeros(K)
-    J = np.zeros(K, dtype=np.int)
+    J = np.zeros(K)
 
     smaller = []
     larger = []
     for kk, prob in enumerate(probs):
         q[kk] = K * prob
-        if q[kk] < 1.0:
+        if q[kk] < 1:
             smaller.append(kk)
         else:
             larger.append(kk)
@@ -104,8 +90,8 @@ def alias_setup(probs):
         large = larger.pop()
 
         J[small] = large
-        q[large] = q[large] + q[small] - 1.0
-        if q[large] < 1.0:
+        q[large] = q[large] + q[small] - 1
+        if q[large] < 1:
             smaller.append(large)
         else:
             larger.append(large)
@@ -113,11 +99,9 @@ def alias_setup(probs):
     return J, q
 
 
-def alias_draw(J, q):
-    K = len(J)
-
-    kk = int(np.floor(np.random.rand() * K))
-    if np.random.rand() < q[kk]:
-        return kk
+def alias_draw(j, q):
+    i = int(random.uniform(0, len(j)))
+    if np.random.rand() < q[i]:
+        return i
     else:
-        return J[kk]
+        return j[i]
